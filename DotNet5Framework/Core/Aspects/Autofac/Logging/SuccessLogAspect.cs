307a26b5a599b67df6e54828385268feb4e29c45
mhmtctn00 <56972953+mhmtctn00@users.noreply.github.com>
2021-07-28 16:38:21 +0300

    SuccessLogAspect Added

---
+++ DotNet5Framework/Core/Aspects/Autofac/Logging/SuccessLogAspect.cs
@@ -0,0 +1,57 @@
+﻿using Castle.DynamicProxy;
+using Core.CrossCuttingConcerns.Logging;
+using Core.Utilities.Interceptors;
+using Core.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Aspects.Autofac.Logging
+{
+    public class SuccessLogAspect : MethodInterception
+    {
+        private LoggerServiceBase _loggerService;
+
+        public SuccessLogAspect(Type loggerService)
+        {
+            if (loggerService.BaseType != typeof(LoggerServiceBase))
+            {
+                throw new System.Exception(AspectMessages.WrongLoggerType);
+            }
+
+            _loggerService = (LoggerServiceBase)Activator.CreateInstance(loggerService);
+        }
+        protected override void OnSuccess(IInvocation invocation)
+        {
+            LogDetail logDetailWithException = GetLogDetail(invocation);
+            _loggerService.Error(logDetailWithException);
+        }
+
+
+        private LogDetailWithException GetLogDetail(IInvocation invocation)
+        {
+            var logParameters = new List<LogParameter>();
+
+            for (int i = 0; i < invocation.Arguments.Length; i++)
+            {
+                logParameters.Add(new LogParameter
+                {
+                    Name = invocation.GetConcreteMethod().GetParameters()[i].Name,
+                    Value = invocation.Arguments[i],
+                    Type = invocation.Arguments[i].GetType().Name
+                });
+            }
+
+            var logDetailWithException = new LogDetailWithException
+            {
+                ClassName = invocation.TargetType.FullName,
+                MethodName = invocation.Method.Name,
+                LogParameters = logParameters
+            };
+
+            return logDetailWithException;
+        }
+    }
+}
