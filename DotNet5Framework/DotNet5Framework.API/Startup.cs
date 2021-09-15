4cfd562d2418927501f1a3c6a36290a364ab769a
mhmtctn00 <56972953+mhmtctn00@users.noreply.github.com>
2021-09-15 22:44:02 +0300

    Static Files Added

--- DotNet5Framework/DotNet5Framework.API/Startup.cs
+++ DotNet5Framework/DotNet5Framework.API/Startup.cs
@@ -9,11 +9,13 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -97,6 +99,12 @@ namespace DotNet5Framework.API
             app.UseDefaultFiles(options);
 
             app.UseStaticFiles();
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"Images")),
+                RequestPath = new Microsoft.AspNetCore.Http.PathString("/Images")
+            });
 
             app.UseRouting();
 
